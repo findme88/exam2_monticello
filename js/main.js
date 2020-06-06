0030d830eec9dbe5a4ce31e73152898b81ba2e34
zordexxx <zordexxx69@gmail.com>
2020-06-06 16:49:58 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40, lng: 73 },
+    zoom: 8,
+  });
+}
+
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    speed: 4000,
+    autoplay: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  });
+});
