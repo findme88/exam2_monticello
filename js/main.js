21d53358c4b2ffb8db494610c144f2727828e80c
zordexxx <zordexxx69@gmail.com>
2020-06-08 10:38:12 +0300

    1.2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 "use strict";
 
-let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40, lng: 73 },
-    zoom: 8,
-  });
-}
 
-$(document).ready(function () {
-  $(".slider").slick({
-    arrows: true,
-    dots: true,
-    adaptiveHeight: true,
-    slidesToShow: 3,
-    speed: 4000,
-    autoplaySpeed: 4000,
-    autoplay: true,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-  });
-});
+// let map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 40, lng: 73 },
+//     zoom: 8,
+//   });
+// }
+
+// $(document).ready(function () {
+    
+//   $(".slider").slick({
+//     arrows: true,
+//     dots: true,
+//     adaptiveHeight: true,
+//     slidesToShow: 3,
+//     speed: 2000,
+//     autoplaySpeed: 4000,
+//     autoplay: true,
+//     // pauseOnFocus: true,
+//     pauseOnHover: true,
+//     pauseOnDotsHover: true,
+//   });
+// });
